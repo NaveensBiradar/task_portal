fdde4ff0e0499a34a1e791f2b20cb9b7e2e2388d
Naveen Biradar <naveensbiradar@gmail.com>
2022-11-18 12:24:47 +0530

    API integration

--- app.js
+++ app.js
@@ -18,17 +18,11 @@ con.connect((err)=>{
     console.log("database connected")
 })
 
-
 //Server listion 
 app.listen(PORT, function(err){
     if (err) throw err;
     console.log("Server is running on the port",PORT);
 })
-
-app.get('/home', (req,res)=>{
-    res.send("GET Request Called HOME page")
-})
-
 
 //Registeration 
 app.post('/register',async (req,res)=>{
@@ -65,43 +59,98 @@ app.post('/login', async (req,res)=>{
         console.log(email, password)
         con.query (`select * from users where email='${email}'`,async function (err, result, fields){
             if (err) throw err;
-            console.log(result[0].password);
-            let hash = result[0].password;
-            await bcrypt.compare(password, hash, function(err, result) {
-                console.log("compare",result)
-                if(result == true){
-                    res.send({message:"success", status:200, data:result})
-                    //redirect to user Dashboard.
-                }else{
-                    res.send({message:"Fails", status:404, data:"Please check the Password"})
-                }
-            });
+            // email validation
+            if(result != ''){
+                console.log("====>",result)
+                console.log(result[0].password);
+                let hash = result[0].password;
+                await bcrypt.compare(password, hash, function(err, result) {
+                    console.log("compare",result)
+                    if(result == true){
+                        res.send({message:"success", status:200, data:result})
+                        //redirect to user Dashboard.
+                    }else{
+                        res.send({message:"Fails", status:404, data:"Please check the Password"})
+                    }
+                });
+            }else{
+                res.send({message:"Fails", status:404, data:"Please check your Email"})
+            }
        })
     }else{
         res.send({message:"Failed", status:400, data:req.body})
     }
 })
 
-app.get('/getTasks_list',(req,res)=>{
-    res.send("GET Request Called Login page")
+
+//Add taskes 
+app.post('/addTasks_list',(req,res)=>{
+    console.log(req.body)
+    let name = req.body.name;
+    let description = req.body.description;
+    let technologies_used = req.body.technologies_used;
+    let time_estimated = req.body.time_estimated;
+    let resoure_allocated = req.body.resoure_allocated;
+    let sql = `INSERT INTO taskList (name,description,technologies_used,time_estimated,resoure_allocated) VALUES ('${name}','${description}','${technologies_used}','${time_estimated}','${resoure_allocated}')`;
+    con.query(sql,(err, result)=>{
+        if (err){
+            res.send({message:"Fails", status:400,data:err})
+        }else{
+            console.log("result=>",result)
+            if(result.affectedRows == 1){
+                res.send({message:"success", status:200,data:req.body})
+            }else{
+                res.send({message:"Fails", status:400})
+            }
+        }
+    })
 })
 
-app.get('/getUserProfile',(req,res)=>{
-    res.send("GET Request Called Login page")
+//Get all Tasks
+app.get('/getTasks_list',(req,res)=>{
+    let sql = `SELECT * FROM taskList`;
+    con.query(sql,(err,result,fields)=>{
+        if(err){
+            res.send({message:"Failed",status:400,data:err})
+        }else{
+            console.log(result);
+            res.send({message:"success",status:200,data:result})
+        }
+    })
 })
+
+//All User List
+app.get('/getAllusers',(req,res)=>{
+    con.query ("select id,f_name,l_name,contact,email from users", function (err, result, fields){
+        if(err){
+            res.send({message:"Failed",status:400,data:err})
+        }else{
+            console.log(result);
+            res.send({message:"success",status:200,data:result})
+        }
+    })    
+})
+
+//Get particular user
+app.get('/getUserProfile',(req,res)=>{
+    let sql = `SELECT * FROM users where id = 1`;
+    con.query(sql,(err,result,fields)=>{
+        if(err){
+            res.send({message:"Failed",status:400,data:err})
+        }else{
+            console.log(result);
+            res.send({message:"success",status:200,data:result})
+        }
+    })
+})
+
 
 app.get('/getTaskStatus',(req,res)=>{
     res.send("GET Request Called Login page")
 })
 
-
-//All User List
-app.get('/getAllusers',(req,res)=>{
-   con.query ("select * from users", function (err, result, fields){
-        if (err) throw err;
-        console.log(result);
-        res.send({message:"success", status:200, data:result})
-   })
+app.get('/deleteTask',(req,res)=>{
+    res.send("GET Request Called Login page")
 })
 
 //delete
@@ -109,6 +158,3 @@ app.get('/deleteUser',(req,res)=>{
     res.send("GET Request Called Login page")
 })
 
-app.get('/deleteTask',(req,res)=>{
-    res.send("GET Request Called Login page")
-})
--- database.js
+++ database.js
@@ -8,14 +8,24 @@ const con = mysql.createConnection({
     database:'Task_portal'
 })
 
+// con.connect(function(err) {
+//     if (err) throw err;
+//     console.log("Connected!");
+//     var sql = "CREATE TABLE users (id int NOT NULL AUTO_INCREMENT,f_name VARCHAR(255),l_name VARCHAR(255),contact VARCHAR(255),email VARCHAR(255),password VARCHAR(255),role VARCHAR(255),PRIMARY KEY (id),UNIQUE (email))";
+//     con.query(sql, function (err, result) {
+//       if (err) throw err;
+//       console.log("Table created");
+//     });
+// });
+
 con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "CREATE TABLE taskList (id int NOT NULL AUTO_INCREMENT,name VARCHAR(255),description VARCHAR(255),technologies_used JSON DEFAULT NULL, time_estimated VARCHAR(255),resoure_allocated VARCHAR(255),PRIMARY KEY (id))";
+  con.query(sql, function (err, result) {
     if (err) throw err;
-    console.log("Connected!");
-    var sql = "CREATE TABLE users (id int NOT NULL AUTO_INCREMENT,f_name VARCHAR(255),l_name VARCHAR(255),contact VARCHAR(255),email VARCHAR(255),password VARCHAR(255),role VARCHAR(255),PRIMARY KEY (id),UNIQUE (email))";
-    con.query(sql, function (err, result) {
-      if (err) throw err;
-      console.log("Table created");
-    });
+    console.log("Table created");
+  });
 });
 
 
